cd3f6dcaaf5fe2672de41d4f9a45948443550457
Adarsh <amlearning2023@gmail.com>
2024-03-10 11:43:51 -0700

    implemented AppFactory, added missed "edit log" comments for Command

--- mvcApps/src/mvc/AppFactory.java
+++ mvcApps/src/mvc/AppFactory.java
@@ -1,5 +1,28 @@
 package mvc;
+import java.util.List;
 
-public class AppFactory {
+/*
+Edits:
+
+    Adarsh 3/10/24: implemented AppFactory (MVC Framework: finished)
+
+ */
+
+public interface AppFactory {
+
+    public Model makeModel();
+
+    public View makeView();
+
+    public String getTitle();
+
+    public List<String> getHelp();
+
+    public String about();
+
+    public List<String> getEditCommands();
+
+    public Command makeEditCommand(String name);
+
 
 }
