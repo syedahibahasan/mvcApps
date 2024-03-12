59d12d342af6ebce528004a79832e51d493819c1
“Hiba <hibahasan151214@gmail.com>
2024-03-11 21:45:10 -0700

    Changed the execute method to an abstract type

--- mvcApps/src/mvc/Command.java
+++ mvcApps/src/mvc/Command.java
@@ -4,12 +4,13 @@ package mvc;
 Edits:
 
     Adarsh 3/10/24: implemented Command (MVC Pattern: finished)
+    
+    Hiba 3/10/24: changed the execute method to an abstract type to enforce subclass-specific implementations
 
  */
 
 public abstract class Command {
+    protected Model model;
 
-    private Model model;
-    public void execute();
-
+    public abstract void execute() throws Exception; // Declare the method as abstract
 }
