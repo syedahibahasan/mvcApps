bd95410110bc650b444a136f4d605b8b9a957e94
Adarsh <amlearning2023@gmail.com>
2024-03-18 15:09:34 -0700

    Created abstract GridFactory class with makeModel() abstract method

---
+++ mvcApps/src/CALab/GridFactory.java
@@ -0,0 +1,14 @@
+package CALab;
+import mvc.*;
+
+/*
+ Edits:
+ 
+ Adarsh 3/18/24: created class
+ 
+ */
+public abstract class GridFactory implements AppFactory {
+
+	public abstract Model makeModel();
+	
+}
