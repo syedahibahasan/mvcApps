dbc33caad1b8cdfd884fa79f9f9370bd7d3ca082
chris768vu <40015054+chris768vu@users.noreply.github.com>
2024-03-17 20:58:51 -0700

    Starting Agent (a customization of Cell for Life)

--- mvcApps/src/Life/Agent.java
+++ mvcApps/src/Life/Agent.java
@@ -1,1 +1,30 @@
+package Life;
+import CALab.Cell;
 
+/*
+ * Christopher Vu 3/17/24: Created Life package, added Agent.java
+ */
+
+public class Agent extends Cell {
+	int status = 0;
+	int ambience = 8;
+	
+	public void update() {
+		
+	}
+	
+	public void observe() {
+		
+	}
+	
+	protected int getStatus() {
+		return status;
+	}
+	
+	public void interact() {}
+	
+	public void reset(boolean randomly) {}
+	
+	public void nextState() {}
+
+}
