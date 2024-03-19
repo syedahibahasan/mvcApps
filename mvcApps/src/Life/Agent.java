b063c4e2e462efd4ec1611192ba1fd6c35d5a5c4
chris768vu <40015054+chris768vu@users.noreply.github.com>
2024-03-18 23:10:11 -0700

    Added Society.java, implemented some methods in Society and Agent

--- mvcApps/src/Life/Agent.java
+++ mvcApps/src/Life/Agent.java
@@ -1,29 +1,48 @@
 package Life;
 import CALab.Cell;
-
+import java.lang.Math;
+import java.util.*;
 /*
  * Christopher Vu 3/17/24: Created Life package, added Agent.java
+ * 
+ * Christopher Vu 3/18/24: implemented update(), observe(), and interact()
  */
 
 public class Agent extends Cell {
-	int status = 0;
-	int ambience = 8;
+	// status == 0 is dead, == 1 is alive
+	private int status = 0;
+	// observe() updates ambience to this Agent's total number of neighbors that are alive
+	private int ambience = 8;
 	
 	public void update() {
-		
+		if (Society.rebirth.contains(this.ambience)) {
+			this.status = 1; 
+		} else if (Society.death.contains(this.ambience)) {
+			this.status = 0;
+		}
 	}
 	
 	public void observe() {
-		
+		int result = 0;
+		Iterator<Cell> neighbor = this.neighbors.iterator();
+		while(neighbor.hasNext()) {
+			if (( (Agent) neighbor).getStatus() == 1) {
+				result++;
+			}
+		}
+		ambience = result;
 	}
 	
 	protected int getStatus() {
 		return status;
 	}
 	
+	// "interaction phase is empty (i.e., a no-op)" - Dr. Pearce
 	public void interact() {}
 	
-	public void reset(boolean randomly) {}
+	public void reset(boolean randomly) {
+		
+	}
 	
 	public void nextState() {}
 
---
+++ mvcApps/src/Life/Society.java
@@ -0,0 +1,32 @@
+package Life;
+import CALab.Grid;
+import java.util.Set;
+import java.util.HashSet;
+
+/*
+ * Christopher Vu 3/18/24: Started Society.java, added static initialization of rebirth and death (from lecture notes)
+ */
+
+public class Society extends Grid {
+	public static int percentAlive = 50;
+	public static Set<Integer> rebirth = new HashSet<Integer>();
+	public static Set<Integer> death = new HashSet<Integer>();
+	
+	public Agent makeCell() {
+		return new Agent();
+	}
+	
+	// initially assigning the ambiances to rebirth or death
+	static {
+	     death.add(0);
+	     death.add(1);
+	     // no change of state for ambiance 2
+	     rebirth.add(3);
+	     death.add(4);
+	     death.add(5);
+	     death.add(6);
+	     death.add(7);
+	     death.add(8);
+	}
+
+}
