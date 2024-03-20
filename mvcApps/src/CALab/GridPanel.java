77e46fd83bae10e89bf29fc0c7c5919c155e1f90
Adarsh <amlearning2023@gmail.com>
2024-03-20 10:25:48 -0700

    Created GridPanel JButtons

--- mvcApps/src/CALab/GridPanel.java
+++ mvcApps/src/CALab/GridPanel.java
@@ -9,10 +9,28 @@ import Life.*;
 
 public class GridPanel extends AppPanel{
 	
-	private JButton thing;
+	private JButton run1;
+	private JButton run50;
+	private JButton populate;
+	private JButton clear;
 	
 	public GridPanel(AppFactory factory) {
 		super(factory);
+		run1 = new JButton("Run 1");
+		run1.addActionListener(this);
+		run50 = new JButton("Run 50");
+		run50.addActionListener(this);
+		populate = new JButton("Populate");
+		populate.addActionListener(this);
+		clear = new JButton("Clear");
+		clear.addActionListener(this);
+		
+		controlPanel.add(run1);
+		controlPanel.add(run50);
+		controlPanel.add(populate);
+		controlPanel.add(clear);
+		
+		
 	}
 	
 	public static void main(String[] args) {
