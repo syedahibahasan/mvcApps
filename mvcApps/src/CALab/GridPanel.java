ea60baf6d85e093ec47cf9227f738bf8a769a1ca
syedahibahasan <hibahasan151214@gmail.com>
2024-03-20 15:42:39 -0700

    Integrated GridView into GridPanel for display.

--- mvcApps/src/CALab/GridPanel.java
+++ mvcApps/src/CALab/GridPanel.java
@@ -2,9 +2,15 @@ package CALab;
 
 import java.awt.*;
 import javax.swing.*;
-
 import mvc.*;
 import Life.*;
+
+/*
+Edits:
+   Hiba 3/15/24: created file
+   
+   Hiba 3/20/24: Integrated GridView into GridPanel for display.
+*/
 
 
 public class GridPanel extends AppPanel{
@@ -16,6 +22,9 @@ public class GridPanel extends AppPanel{
 	
 	public GridPanel(AppFactory factory) {
 		super(factory);
+	    View gridView = factory.makeView(model);
+	    add(gridView); 
+	 
 		run1 = new JButton("Run 1");
 		run1.addActionListener(this);
 		run50 = new JButton("Run 50");
