1d96dd411d61b3adf3fb52fa0de6284b86106ebd
syedahibahasan <hibahasan151214@gmail.com>
2024-03-20 02:49:26 -0700

    Implemented GridView to display a grid layout

--- mvcApps/src/CALab/GridView.java
+++ mvcApps/src/CALab/GridView.java
@@ -7,6 +7,9 @@ import java.awt.*;
 /*
 Edits:
    Hiba 3/15/24: created file
+   
+   Hiba:3/20/24: Implemented GridView to display a grid layout corresponding to the dimensions of the model's grid. 
+   				 Initialized and populated CellView for each cell in the grid.
 */
 
 public class GridView extends View {
@@ -15,21 +18,29 @@ public class GridView extends View {
     protected Grid model;
 
     public GridView(Model model) {
-    	super(model);
-        /*
-        Cell cell = new CellView(((Grid)model).getCell(row, col)
-        cellViews[row][col] = cell
-        set cell.row and cell.col here
-        */
+        super(model);
+        this.model = (Grid) model; 
+        setLayout(new GridLayout(this.model.getDim(), this.model.getDim())); 
+
+        // Initialize cellViews with the dimensions of the grid
+        cellViews = new CellView[this.model.getDim()][this.model.getDim()];
+        
+        // Populate cellViews with CellView objects
+        for (int i = 0; i < this.model.getDim(); i++) {
+            for (int j = 0; j < this.model.getDim(); j++) {
+                Cell cell = this.model.getCell(i, j);
+                cellViews[i][j] = new CellView(cell);
+                add(cellViews[i][j]); // Add CellView to the GridView
+            }
+        }
     }
 
     public void update() {
         // call update method of each CellView
     	for (int i = 0; i < cellViews.length; i++) {
             for (int j = 0; j < cellViews[i].length; j++) {
-                cellViews[i][j].update(); 
+                cellViews[i][j].update();
             }
         }
     }
-
 }
